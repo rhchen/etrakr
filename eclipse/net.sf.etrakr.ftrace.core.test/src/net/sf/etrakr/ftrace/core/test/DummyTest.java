c07ad2b358c26548eed0e314c5c756022eb07b1a
rhchen <ranhow.chen@gmail.com>
2015-01-03 15:39:40 +0800

    Add net.sf.etrakr.ftrace.core.test plugin
    For ftrace service test
    
    	modified:   eclipse/net.sf.etrakr.aggregator/pom.xml
    	new file:   eclipse/net.sf.etrakr.ftrace.core.test/.classpath
    	new file:   eclipse/net.sf.etrakr.ftrace.core.test/.project
    	new file:   eclipse/net.sf.etrakr.ftrace.core.test/.settings/org.eclipse.jdt.core.prefs
    	new file:   eclipse/net.sf.etrakr.ftrace.core.test/.settings/org.eclipse.m2e.core.prefs
    	new file:   eclipse/net.sf.etrakr.ftrace.core.test/.settings/org.eclipse.pde.core.prefs
    	new file:   eclipse/net.sf.etrakr.ftrace.core.test/META-INF/MANIFEST.MF
    	new file:   eclipse/net.sf.etrakr.ftrace.core.test/build.properties
    	new file:   eclipse/net.sf.etrakr.ftrace.core.test/data/android_systrace.txt
    	new file:   eclipse/net.sf.etrakr.ftrace.core.test/pom.xml
    	new file:   eclipse/net.sf.etrakr.ftrace.core.test/src/net/sf/etrakr/ftrace/core/test/DummyTest.java
    	modified:   lib/com.fasterxml.jackson.test/pom.xml

---
+++ eclipse/net.sf.etrakr.ftrace.core.test/src/net/sf/etrakr/ftrace/core/test/DummyTest.java
@@ -0,0 +1,71 @@
+package net.sf.etrakr.ftrace.core.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import net.sf.etrakr.ftrace.core.service.IFtraceService;
+import net.sf.etrakr.ftrace.core.service.impl.FtraceService;
+
+import org.eclipse.linuxtools.tmf.core.event.ITmfEvent;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.TreeBasedTable;
+
+public class DummyTest {
+
+	private static final File _file = new File("data/android_systrace.txt");
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() throws IOException {
+		
+		IFtraceService service = new FtraceService();
+		
+		Assert.assertNotNull(service);
+		
+		service.addTrace(_file.toURI());
+		
+		TreeBasedTable<Integer, Long, Long> pageTable = service.getPageTable(_file.toURI());
+		
+		int pages = pageTable.size();
+		
+		BiMap<Long, Integer> rankTable = service.getRankTable(_file.toURI());
+		
+		Iterator<Entry<Long, Integer>> it = rankTable.entrySet().iterator();
+		
+		while(it.hasNext()){
+			
+			Entry<Long, Integer> entry = it.next();
+			
+			String entryData = entry.getKey() +" "+ entry.getValue();
+			
+			System.out.println("IFtraceService entryData = "+ entryData);
+		}
+	}
+	
+}
