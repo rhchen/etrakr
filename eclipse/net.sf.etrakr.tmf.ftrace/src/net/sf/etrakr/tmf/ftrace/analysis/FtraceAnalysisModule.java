26911ce874eef1ff05dc5ebd40d4c969654f9119
rhchen <ranhow.chen@gmail.com>
2015-01-03 16:50:01 +0800

    Add net.sf.etrakr.tmf.ftrace plugin
    	modified:   eclipse/net.sf.etrakr.aggregator/pom.xml
    	modified:   eclipse/net.sf.etrakr.feature/feature.xml
    	modified:   eclipse/net.sf.etrakr.ftrace.core/META-INF/MANIFEST.MF
    	new file:   eclipse/net.sf.etrakr.tmf.ftrace/.classpath
    	new file:   eclipse/net.sf.etrakr.tmf.ftrace/.project
    	new file:   eclipse/net.sf.etrakr.tmf.ftrace/.settings/org.eclipse.jdt.core.prefs
    	new file:   eclipse/net.sf.etrakr.tmf.ftrace/.settings/org.eclipse.m2e.core.prefs
    	new file:   eclipse/net.sf.etrakr.tmf.ftrace/.settings/org.eclipse.pde.core.prefs
    	new file:   eclipse/net.sf.etrakr.tmf.ftrace/META-INF/MANIFEST.MF
    	new file:   eclipse/net.sf.etrakr.tmf.ftrace/build.properties
    	new file:   eclipse/net.sf.etrakr.tmf.ftrace/plugin.xml
    	new file:   eclipse/net.sf.etrakr.tmf.ftrace/pom.xml
    	new file:   eclipse/net.sf.etrakr.tmf.ftrace/src/net/sf/etrakr/tmf/ftrace/TmfFtraceActivator.java
    	new file:   eclipse/net.sf.etrakr.tmf.ftrace/src/net/sf/etrakr/tmf/ftrace/analysis/FtraceAnalysisModule.java
    	new file:   eclipse/net.sf.etrakr.tmf.ftrace/src/net/sf/etrakr/tmf/ftrace/state/Attributes.java
    	new file:   eclipse/net.sf.etrakr.tmf.ftrace/src/net/sf/etrakr/tmf/ftrace/state/FtraceStateProvider.java
    	new file:   eclipse/net.sf.etrakr.tmf.ftrace/src/net/sf/etrakr/tmf/ftrace/state/StateValues.java
    	new file:   eclipse/net.sf.etrakr.tmf.ftrace/src/net/sf/etrakr/tmf/ftrace/trace/FtraceTrace.java

---
+++ eclipse/net.sf.etrakr.tmf.ftrace/src/net/sf/etrakr/tmf/ftrace/analysis/FtraceAnalysisModule.java
@@ -0,0 +1,105 @@
+/*******************************************************************************
+ * Copyright (c) 2013, 2014 École Polytechnique de Montréal
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Geneviève Bastien - Initial API and implementation
+ *   Mathieu Rail - Provide the requirements of the analysis
+ *******************************************************************************/
+
+package net.sf.etrakr.tmf.ftrace.analysis;
+
+import net.sf.etrakr.ftrace.core.FtraceStrings;
+import net.sf.etrakr.tmf.ftrace.state.FtraceStateProvider;
+
+import org.eclipse.linuxtools.tmf.core.analysis.TmfAnalysisRequirement;
+import org.eclipse.linuxtools.tmf.core.analysis.TmfAnalysisRequirement.ValuePriorityLevel;
+import org.eclipse.linuxtools.tmf.core.statesystem.ITmfStateProvider;
+import org.eclipse.linuxtools.tmf.core.statesystem.TmfStateSystemAnalysisModule;
+
+import com.google.common.collect.ImmutableSet;
+
+/**
+ * State System Module for lttng kernel traces
+ *
+ * @author Geneviève Bastien
+ * @since 3.0
+ */
+public class FtraceAnalysisModule extends TmfStateSystemAnalysisModule {
+
+    /**
+     * The file name of the History Tree
+     */
+    public static final String HISTORY_TREE_FILE_NAME = "stateHistory.ht"; //$NON-NLS-1$
+
+    /** The ID of this analysis module */
+    public static final String ID = "net.sf.notrace.ftrace.analysis.FtraceAnalysisModule"; //$NON-NLS-1$
+
+    /*
+     * TODO: Decide which events should be mandatory for the analysis, once the
+     * appropriate error messages and session setup are in place.
+     */
+    private static final ImmutableSet<String> REQUIRED_EVENTS = ImmutableSet.of();
+
+    private static final ImmutableSet<String> OPTIONAL_EVENTS = ImmutableSet.of(
+    		FtraceStrings.EXIT_SYSCALL,
+    		FtraceStrings.IRQ_HANDLER_ENTRY,
+    		FtraceStrings.IRQ_HANDLER_EXIT,
+    		FtraceStrings.SOFTIRQ_ENTRY,
+    		FtraceStrings.SOFTIRQ_EXIT,
+    		FtraceStrings.SOFTIRQ_RAISE,
+    		FtraceStrings.SCHED_PROCESS_FORK,
+    		FtraceStrings.SCHED_PROCESS_EXIT,
+    		FtraceStrings.SCHED_PROCESS_FREE,
+    		FtraceStrings.SCHED_SWITCH,
+    		FtraceStrings.STATEDUMP_PROCESS_STATE,
+    		FtraceStrings.SCHED_WAKEUP,
+            FtraceStrings.SCHED_WAKEUP_NEW,
+
+            /* FIXME Add the prefix for syscalls */
+            FtraceStrings.SYSCALL_PREFIX
+            );
+
+    /** The requirements as an immutable set */
+    private static final ImmutableSet<TmfAnalysisRequirement> REQUIREMENTS;
+
+    /* Since not tend to depend on SessionConfigStrings, move the define here */
+    private static final String CONFIG_ELEMENT_DOMAIN = "domain";
+    private static final String CONFIG_DOMAIN_TYPE_KERNEL = "KERNEL";
+    private static final String CONFIG_ELEMENT_EVENT = "event";
+    
+    static {
+        /* initialize the requirement: domain and events */
+        TmfAnalysisRequirement domainReq = new TmfAnalysisRequirement(CONFIG_ELEMENT_DOMAIN);
+        domainReq.addValue(CONFIG_DOMAIN_TYPE_KERNEL, ValuePriorityLevel.MANDATORY);
+
+        TmfAnalysisRequirement eventReq = new TmfAnalysisRequirement(CONFIG_ELEMENT_EVENT, REQUIRED_EVENTS, ValuePriorityLevel.MANDATORY);
+        eventReq.addValues(OPTIONAL_EVENTS, ValuePriorityLevel.OPTIONAL);
+
+        REQUIREMENTS = ImmutableSet.of(domainReq, eventReq);
+    }
+
+    @Override
+    protected ITmfStateProvider createStateProvider() {
+        return new FtraceStateProvider(getTrace());
+    }
+
+    @Override
+    protected String getSsFileName() {
+        return HISTORY_TREE_FILE_NAME;
+    }
+
+    @Override
+    protected String getFullHelpText() {
+        return "Builds the Ftrace kernel state system to populate the Control Flow view and the Resources View";
+    }
+
+    @Override
+    public Iterable<TmfAnalysisRequirement> getAnalysisRequirements() {
+        return REQUIREMENTS;
+    }
+}
---
+++ eclipse/net.sf.etrakr.tmf.ftrace/src/net/sf/etrakr/tmf/ftrace/trace/FtraceTrace.java
@@ -0,0 +1,168 @@
+package net.sf.etrakr.tmf.ftrace.trace;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+import net.sf.etrakr.ftrace.core.event.IFtraceEvent;
+import net.sf.etrakr.ftrace.core.service.IFtraceService;
+import net.sf.etrakr.ftrace.core.service.impl.FtraceService;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.linuxtools.tmf.core.event.ITmfEvent;
+import org.eclipse.linuxtools.tmf.core.exceptions.TmfTraceException;
+import org.eclipse.linuxtools.tmf.core.trace.ITmfContext;
+import org.eclipse.linuxtools.tmf.core.trace.ITmfEventParser;
+import org.eclipse.linuxtools.tmf.core.trace.TmfContext;
+import org.eclipse.linuxtools.tmf.core.trace.TmfTrace;
+import org.eclipse.linuxtools.tmf.core.trace.location.ITmfLocation;
+import org.eclipse.linuxtools.tmf.core.trace.location.TmfLongLocation;
+
+public class FtraceTrace extends TmfTrace implements ITmfEventParser {
+
+	public static final String PLUGIN_ID = "net.sf.notrace.ftrace";
+	
+	private static final TmfLongLocation NULLLOCATION = new TmfLongLocation((Long) null);
+	
+	private static final TmfContext NULLCONTEXT = new TmfContext(NULLLOCATION,-1L);
+	
+	private IFtraceService service = new FtraceService();
+	
+	private File fFile;
+	
+	private TmfLongLocation fCurrentLocation;
+	
+	@Override
+	public IStatus validate(IProject project, String path) {
+		
+		File f = new File(path);
+		if (!f.exists()) {
+			return new Status(IStatus.ERROR, PLUGIN_ID, "File does not exist"); //$NON-NLS-1$
+		}
+		if (!f.isFile()) {
+			return new Status(IStatus.ERROR, PLUGIN_ID, path + " is not a file"); //$NON-NLS-1$
+		}
+		
+		return Status.OK_STATUS;
+	}
+
+	@Override
+	public void initTrace(IResource resource, String path,
+			Class<? extends ITmfEvent> type) throws TmfTraceException {
+		
+		super.initTrace(resource, path, type);
+		
+		fFile = new File(path);
+		
+		try {
+			
+			long fSize = FtraceService.getFileSize(fFile.toURI());
+			
+			if (fSize == 0) throw new TmfTraceException("file is empty"); //$NON-NLS-1$
+			
+			int nbEvents = 1 + (int)fSize/1024/1024;
+			
+			/* A guess to number of events */
+			setNbEvents(nbEvents * 10000);
+			
+			if (getNbEvents() < 1) throw new TmfTraceException("Trace does not have any events"); //$NON-NLS-1$
+
+			service.addTrace(fFile.toURI());
+			
+			/* trigger service to load data into cache */
+			seek(0);
+			
+		} catch (IOException e) {
+			
+			throw new TmfTraceException(e.getMessage(), e);
+			
+		} catch (ExecutionException e) {
+			
+			throw new TmfTraceException(e.getMessage(), e);
+		}
+	
+		
+	}
+
+	@Override
+	public ITmfLocation getCurrentLocation() {
+		return fCurrentLocation;
+	}
+
+	@Override
+	public double getLocationRatio(ITmfLocation location) {
+		return ((TmfLongLocation) location).getLocationInfo().doubleValue() / getNbEvents();
+	}
+
+	@Override
+	public ITmfContext seekEvent(ITmfLocation location) {
+		
+		TmfLongLocation nl = (TmfLongLocation) location;
+		
+		if (location == null) nl = new TmfLongLocation(0L);
+		
+		try {
+			
+			seek(nl.getLocationInfo());
+		
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+		
+		return new TmfContext(nl, nl.getLocationInfo());
+	}
+
+	@Override
+	public ITmfContext seekEvent(double ratio) {
+		
+		long rank = (long) (ratio * getNbEvents());
+		
+		try {
+			
+			seek(rank);
+		
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+		
+		return new TmfContext(new TmfLongLocation(rank), rank);
+	}
+
+	@Override
+	public ITmfEvent parseEvent(ITmfContext context) {
+		
+		if ((context == null) || (context.getRank() == -1)) return null;
+
+		long pos = context.getRank();
+		
+		/* Escape on count == NBevents */
+		if(pos == getNbEvents()) return null;
+		
+		fCurrentLocation = new TmfLongLocation(pos);
+		
+		try {
+			
+			ITmfEvent event = service.getTmfEvent(fFile.toURI(), pos);
+
+			if(event == null) return null;
+			
+			event = ((IFtraceEvent) event).newEvent(this);
+			
+			return event;
+			
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+	private void seek(long rank) throws ExecutionException{
+		
+		service.getTmfEvent(fFile.toURI(), rank);
+		
+	}
+}
