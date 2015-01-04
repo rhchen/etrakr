50984e18a8d5913772d6b9cb77187994dbb16082
rhchen <ranhow.chen@gmail.com>
2015-01-04 13:07:34 +0800

    Add net.sf.etrakr.trans.ftrace.test plugin
    For test json transform
    
    	modified:   eclipse/net.sf.etrakr.aggregator/pom.xml
    	new file:   eclipse/net.sf.etrakr.trans.ftrace.test/.classpath
    	new file:   eclipse/net.sf.etrakr.trans.ftrace.test/.project
    	new file:   eclipse/net.sf.etrakr.trans.ftrace.test/.settings/org.eclipse.jdt.core.prefs
    	new file:   eclipse/net.sf.etrakr.trans.ftrace.test/.settings/org.eclipse.m2e.core.prefs
    	new file:   eclipse/net.sf.etrakr.trans.ftrace.test/.settings/org.eclipse.pde.core.prefs
    	new file:   eclipse/net.sf.etrakr.trans.ftrace.test/META-INF/MANIFEST.MF
    	new file:   eclipse/net.sf.etrakr.trans.ftrace.test/build.properties
    	new file:   eclipse/net.sf.etrakr.trans.ftrace.test/data/android_systrace.txt
    	new file:   eclipse/net.sf.etrakr.trans.ftrace.test/pom.xml
    	new file:   eclipse/net.sf.etrakr.trans.ftrace.test/src/net/sf/etrakr/trans/ftrace/test/DummyTest.java
    	modified:   pom.xml

---
+++ eclipse/net.sf.etrakr.trans.ftrace.test/src/net/sf/etrakr/trans/ftrace/test/DummyTest.java
@@ -0,0 +1,81 @@
+package net.sf.etrakr.trans.ftrace.test;
+
+import java.io.File;
+import java.io.IOException;
+
+import net.sf.etrakr.trans.ftrace.deser.FtraceDeserializer;
+import net.sf.etrakr.trans.ftrace.module.FtraceMapper;
+import net.sf.etrakr.trans.ftrace.module.FtraceModule;
+
+import org.eclipse.linuxtools.tmf.core.event.ITmfEvent;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.TreeBasedTable;
+
+public class DummyTest {
+
+	private static final File _file = new File("data/android_systrace.txt");
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public <T> void test_1() throws JsonProcessingException, IOException {
+		
+		if(!_file.exists()) throw new IOException("File not exist");
+		
+		FtraceDeserializer deserializer = new FtraceDeserializer();
+		
+		FtraceModule module = new FtraceModule();
+		
+		module.addDeserializer(ITmfEvent.class, deserializer);
+		
+		FtraceMapper mapper = new FtraceMapper();
+		
+		mapper.registerModule(module);
+		
+		MappingIterator<T> it = mapper.reader(ITmfEvent.class).readValues(_file);
+		
+		while (it.hasNext()) {
+			
+			@SuppressWarnings("unused")
+			T row = it.nextValue();
+
+			if(row instanceof ITmfEvent){
+				
+				ITmfEvent ev = (ITmfEvent) row;
+				
+				String source = ev.getSource();
+				String ref = ev.getReference();
+				long rank = ev.getRank();
+				
+				String event = rank +" : "+ source  +" : "+ ref;
+				
+				//System.out.println("event : "+ event);
+			}
+		}
+	}
+
+}
