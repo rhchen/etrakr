0991a88d19cfdedb3d33d6d03c1595fbed5d5e03
ranhow.chen <ranhow.chen@gmail.com>
2015-07-19 16:22:25 +0800

    Enable adb device connect detection
    	modified:   lib/com.android.ddmlib/META-INF/MANIFEST.MF
    	modified:   rcp/net.sf.etrakr.feature/feature.xml
    	modified:   rcp/net.sf.etrakr.repository/etrakr.product
    	modified:   tmf/net.sf.etrakr.tmf.adb.core/META-INF/MANIFEST.MF
    	modified:   tmf/net.sf.etrakr.tmf.adb.core/src/net/sf/etrakr/tmf/adb/core/AdbActivator.java

--- tmf/net.sf.etrakr.tmf.adb.core/src/net/sf/etrakr/tmf/adb/core/AdbActivator.java
+++ tmf/net.sf.etrakr.tmf.adb.core/src/net/sf/etrakr/tmf/adb/core/AdbActivator.java
@@ -1,10 +1,24 @@
 package net.sf.etrakr.tmf.adb.core;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-public class AdbActivator implements BundleActivator {
+import com.android.ddmlib.AndroidDebugBridge;
+import com.android.ddmlib.AndroidDebugBridge.IClientChangeListener;
+import com.android.ddmlib.AndroidDebugBridge.IDebugBridgeChangeListener;
+import com.android.ddmlib.AndroidDebugBridge.IDeviceChangeListener;
+import com.android.ddmlib.Client;
+import com.android.ddmlib.IDevice;
 
+public class AdbActivator implements BundleActivator, IDebugBridgeChangeListener, IDeviceChangeListener, IClientChangeListener{
+
+	public static final String DdmsPlugin_DDMS_Post_Create_Init = "DDMS post-create init";
+	
 	private static BundleContext context;
 
 	static BundleContext getContext() {
@@ -16,7 +30,29 @@ public class AdbActivator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
+		
 		AdbActivator.context = bundleContext;
+		
+		System.out.println("AdbActivator.start : DDMS post-create init");
+		
+		AndroidDebugBridge.addDebugBridgeChangeListener(this);
+        AndroidDebugBridge.addDeviceChangeListener(this);
+        AndroidDebugBridge.addClientChangeListener(this);
+        
+        new Job(DdmsPlugin_DDMS_Post_Create_Init) {
+        	
+            @Override
+            protected IStatus run(IProgressMonitor monitor) {
+            	
+            	// init the lib
+                AndroidDebugBridge.init(false /* debugger support */);
+
+                AndroidDebugBridge.createBridge("adb", false /* forceNewBridge */);
+
+                return Status.OK_STATUS;
+            }
+            
+        }.schedule();
 	}
 
 	/*
@@ -25,6 +61,36 @@ public class AdbActivator implements BundleActivator {
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
 		AdbActivator.context = null;
+	}
+
+	@Override
+	public void clientChanged(Client client, int changeMask) {
+		System.out.println("AdbActivator.clientChanged");
+		
+	}
+
+	@Override
+	public void deviceConnected(IDevice device) {
+		System.out.println("AdbActivator.deviceConnected");
+		
+	}
+
+	@Override
+	public void deviceDisconnected(IDevice device) {
+		System.out.println("AdbActivator.deviceDisconnected");
+		
+	}
+
+	@Override
+	public void deviceChanged(IDevice device, int changeMask) {
+		System.out.println("AdbActivator.deviceChanged");
+		
+	}
+
+	@Override
+	public void bridgeChanged(AndroidDebugBridge bridge) {
+		System.out.println("AdbActivator.bridgeChanged");
+		
 	}
 
 }
