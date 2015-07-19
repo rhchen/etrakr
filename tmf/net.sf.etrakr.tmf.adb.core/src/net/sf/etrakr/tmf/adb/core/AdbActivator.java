4031d3ae67abe504a08afc9c8e1e236d604cdacf
ranhow.chen <ranhow.chen@gmail.com>
2015-07-19 15:25:36 +0800

    Add TMF remote access via adb
    	modified:   pom.xml
    	new file:   tmf/net.sf.etrakr.tmf.adb.core/.classpath
    	new file:   tmf/net.sf.etrakr.tmf.adb.core/.project
    	new file:   tmf/net.sf.etrakr.tmf.adb.core/.settings/org.eclipse.core.resources.prefs
    	new file:   tmf/net.sf.etrakr.tmf.adb.core/.settings/org.eclipse.jdt.core.prefs
    	new file:   tmf/net.sf.etrakr.tmf.adb.core/.settings/org.eclipse.m2e.core.prefs
    	new file:   tmf/net.sf.etrakr.tmf.adb.core/META-INF/MANIFEST.MF
    	new file:   tmf/net.sf.etrakr.tmf.adb.core/build.properties
    	new file:   tmf/net.sf.etrakr.tmf.adb.core/pom.xml
    	new file:   tmf/net.sf.etrakr.tmf.adb.core/src/net/sf/etrakr/tmf/adb/core/AdbActivator.java
    	new file:   tmf/pom.xml

---
+++ tmf/net.sf.etrakr.tmf.adb.core/src/net/sf/etrakr/tmf/adb/core/AdbActivator.java
@@ -0,0 +1,30 @@
+package net.sf.etrakr.tmf.adb.core;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class AdbActivator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		AdbActivator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		AdbActivator.context = null;
+	}
+
+}
