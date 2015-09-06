8e4f1dcf425f54b274ea65e71aa046b0349c0412
ranhow.chen <ranhow.chen@gmail.com>
2015-09-06 22:57:16 +0800

    Fix ctrace plugin
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/.classpath
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/.project
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/.settings/org.eclipse.core.resources.prefs
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/.settings/org.eclipse.jdt.core.prefs
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/.settings/org.eclipse.m2e.core.prefs
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/META-INF/MANIFEST.MF
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/build.properties
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/icons/eview16/control_flow_view.gif
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/icons/eview16/resources_view.gif
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/icons/obj16/garland16.png
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/plugin.xml
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/pom.xml
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/src/net/sf/etrakr/ctrace/ui/CtracePlugin.java
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/src/net/sf/etrakr/ctrace/ui/Messages.java
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/src/net/sf/etrakr/ctrace/ui/analysis/TmfAnalysisViewOutput.java
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/src/net/sf/etrakr/ctrace/ui/messages.properties
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/src/net/sf/etrakr/ctrace/ui/views/controlflow/ControlFlowEntry.java
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/src/net/sf/etrakr/ctrace/ui/views/controlflow/ControlFlowPresentationProvider.java
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/src/net/sf/etrakr/ctrace/ui/views/controlflow/ControlFlowView.java
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/src/net/sf/etrakr/ctrace/ui/views/cpu/CpuUsageComposite.java
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/src/net/sf/etrakr/ctrace/ui/views/cpu/CpuUsageEntry.java
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/src/net/sf/etrakr/ctrace/ui/views/cpu/CpuUsageView.java
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/src/net/sf/etrakr/ctrace/ui/views/cpu/CpuUsageXYViewer.java
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/src/net/sf/etrakr/ctrace/ui/views/cpu/Messages.java
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/src/net/sf/etrakr/ctrace/ui/views/cpu/messages.properties
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/src/net/sf/etrakr/ctrace/ui/views/resource/ResourcesEntry.java
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/src/net/sf/etrakr/ctrace/ui/views/resource/ResourcesPresentationProvider.java
    	new file:   ctrace/net.sf.etrakr.ctrace.ui/src/net/sf/etrakr/ctrace/ui/views/resource/ResourcesView.java
    	new file:   ctrace/net.sf.etrakr.tmf.ctrace.test/.classpath
    	new file:   ctrace/net.sf.etrakr.tmf.ctrace.test/.project
    	new file:   ctrace/net.sf.etrakr.tmf.ctrace.test/.settings/org.eclipse.core.resources.prefs
    	new file:   ctrace/net.sf.etrakr.tmf.ctrace.test/.settings/org.eclipse.jdt.core.prefs
    	new file:   ctrace/net.sf.etrakr.tmf.ctrace.test/.settings/org.eclipse.m2e.core.prefs
    	new file:   ctrace/net.sf.etrakr.tmf.ctrace.test/META-INF/MANIFEST.MF
    	new file:   ctrace/net.sf.etrakr.tmf.ctrace.test/build.properties
    	new file:   ctrace/net.sf.etrakr.tmf.ctrace.test/data/perf_sampling_trace_with_trace_events.json
    	new file:   ctrace/net.sf.etrakr.tmf.ctrace.test/pom.xml
    	new file:   ctrace/net.sf.etrakr.tmf.ctrace.test/src/net/sf/etrakr/tmf/ctrace/test/DummyTest.java
    	new file:   ctrace/net.sf.etrakr.tmf.ctrace.test/src/net/sf/etrakr/tmf/ctrace/test/StateSystemAnalysisModuleTest.java
    	modified:   ctrace/net.sf.etrakr.tmf.ctrace/META-INF/MANIFEST.MF
    	modified:   ctrace/net.sf.etrakr.tmf.ctrace/build.properties
    	new file:   ctrace/net.sf.etrakr.tmf.ctrace/plugin.xml
    	modified:   ctrace/pom.xml

---
+++ ctrace/net.sf.etrakr.ctrace.ui/src/net/sf/etrakr/ctrace/ui/views/controlflow/ControlFlowPresentationProvider.java
@@ -0,0 +1,220 @@
+/*******************************************************************************
+ * Copyright (c) 2012, 2014 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Patrick Tasse - Initial API and implementation
+ *******************************************************************************/
+
+package net.sf.etrakr.ctrace.ui.views.controlflow;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.sf.etrakr.ctrace.ui.CtracePlugin;
+import net.sf.etrakr.ctrace.ui.Messages;
+import net.sf.etrakr.tmf.ctrace.analysis.CtraceAnalysisModule;
+import net.sf.etrakr.tmf.ctrace.state.Attributes;
+//import net.sf.etrakr.tmf.chrome.state.StateValues;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.tracecompass.statesystem.core.ITmfStateSystem;
+import org.eclipse.tracecompass.statesystem.core.exceptions.AttributeNotFoundException;
+import org.eclipse.tracecompass.statesystem.core.exceptions.StateSystemDisposedException;
+import org.eclipse.tracecompass.statesystem.core.exceptions.StateValueTypeException;
+import org.eclipse.tracecompass.statesystem.core.exceptions.TimeRangeException;
+import org.eclipse.tracecompass.statesystem.core.interval.ITmfStateInterval;
+import org.eclipse.tracecompass.statesystem.core.statevalue.ITmfStateValue;
+import org.eclipse.tracecompass.tmf.core.statesystem.TmfStateSystemAnalysisModule;
+import org.eclipse.tracecompass.tmf.ui.widgets.timegraph.StateItem;
+import org.eclipse.tracecompass.tmf.ui.widgets.timegraph.TimeGraphPresentationProvider;
+import org.eclipse.tracecompass.tmf.ui.widgets.timegraph.model.ITimeEvent;
+import org.eclipse.tracecompass.tmf.ui.widgets.timegraph.model.TimeEvent;
+
+import com.google.common.collect.Maps;
+
+/**
+ * Presentation provider for the control flow view
+ */
+public class ControlFlowPresentationProvider extends TimeGraphPresentationProvider {
+
+    private enum State {
+        UNKNOWN        (new RGB(100, 100, 100)),
+        WAIT_BLOCKED   (new RGB(200, 200, 0)),
+        WAIT_FOR_CPU   (new RGB(200, 100, 0)),
+        USERMODE       (new RGB(0,   200, 0)),
+        SYSCALL        (new RGB(0,     0, 200)),
+        INTERRUPTED    (new RGB(200,   0, 100));
+
+        public final RGB rgb;
+
+        private State(RGB rgb) {
+            this.rgb = rgb;
+        }
+
+    }
+
+    /**
+     * Default constructor
+     */
+    public ControlFlowPresentationProvider() {
+        super(Messages.ControlFlowView_stateTypeName);
+    }
+
+    private static State[] getStateValues() {
+        return State.values();
+    }
+
+    @Override
+    public StateItem[] getStateTable() {
+        State[] states = getStateValues();
+        StateItem[] stateTable = new StateItem[states.length];
+        for (int i = 0; i < stateTable.length; i++) {
+            State state = states[i];
+            stateTable[i] = new StateItem(state.rgb, state.toString());
+        }
+        return stateTable;
+    }
+
+    @Override
+    public int getStateTableIndex(ITimeEvent event) {
+        if (event instanceof TimeEvent && ((TimeEvent) event).hasValue()) {
+            int status = ((TimeEvent) event).getValue();
+            return getMatchingState(status).ordinal();
+        }
+        return TRANSPARENT;
+    }
+
+    @Override
+    public String getEventName(ITimeEvent event) {
+        if (event instanceof TimeEvent) {
+            TimeEvent ev = (TimeEvent) event;
+            if (ev.hasValue()) {
+                return getMatchingState(ev.getValue()).toString();
+            }
+        }
+        return Messages.ControlFlowView_multipleStates;
+    }
+
+    private static State getMatchingState(int status) {
+        switch (status) {
+        /* Fix me, RH */
+//        case StateValues.PROCESS_STATUS_WAIT_BLOCKED:
+//            return State.WAIT_BLOCKED;
+//        case StateValues.PROCESS_STATUS_WAIT_FOR_CPU:
+//            return State.WAIT_FOR_CPU;
+//        case StateValues.PROCESS_STATUS_RUN_USERMODE:
+//            return State.USERMODE;
+//        case StateValues.PROCESS_STATUS_RUN_SYSCALL:
+//            return State.SYSCALL;
+//        case StateValues.PROCESS_STATUS_INTERRUPTED:
+//            return State.INTERRUPTED;
+        default:
+            return State.UNKNOWN;
+        }
+    }
+
+    @Override
+    public Map<String, String> getEventHoverToolTipInfo(ITimeEvent event) {
+    	
+        Map<String, String> retMap = Maps.<String, String>newLinkedHashMap();
+        if (!(event instanceof TimeEvent) || !((TimeEvent) event).hasValue() ||
+                !(event.getEntry() instanceof ControlFlowEntry)) {
+            return retMap;
+        }
+        ControlFlowEntry entry = (ControlFlowEntry) event.getEntry();
+        ITmfStateSystem ssq = TmfStateSystemAnalysisModule.getStateSystem(entry.getTrace(), CtraceAnalysisModule.ID);
+        if (ssq == null) {
+            return retMap;
+        }
+        int tid = entry.getThreadId();
+
+        try {
+            // Find every CPU first, then get the current thread
+            int threadsQuark = ssq.getQuarkAbsolute(Attributes.THREADS);
+            int currentThreadQuark = ssq.getQuarkRelative(threadsQuark, String.valueOf(tid));
+            ITmfStateInterval interval = ssq.querySingleState(event.getTime(), currentThreadQuark);
+            if (!interval.getStateValue().isNull()) {
+                ITmfStateValue state = interval.getStateValue();
+                int currentThreadId = state.unboxInt();
+                if (tid == currentThreadId) {
+                    retMap.put(Messages.ControlFlowView_attributeCpuName, ssq.getAttributeName(currentThreadQuark));
+                }
+            }
+
+        } catch (AttributeNotFoundException e) {
+        	CtracePlugin.getDefault().logError("Error in ControlFlowPresentationProvider", e); //$NON-NLS-1$
+        } catch (TimeRangeException e) {
+        	CtracePlugin.getDefault().logError("Error in ControlFlowPresentationProvider", e); //$NON-NLS-1$
+        } catch (StateValueTypeException e) {
+        	CtracePlugin.getDefault().logError("Error in ControlFlowPresentationProvider", e); //$NON-NLS-1$
+        } catch (StateSystemDisposedException e) {
+            /* Ignored */
+        }
+        int status = ((TimeEvent) event).getValue();
+        /* Fix me, RH */
+//        if (status == StateValues.PROCESS_STATUS_RUN_SYSCALL) {
+//            try {
+//                int syscallQuark = ssq.getQuarkRelative(entry.getThreadQuark(), Attributes.SYSTEM_CALL);
+//                ITmfStateInterval value = ssq.querySingleState(event.getTime(), syscallQuark);
+//                if (!value.getStateValue().isNull()) {
+//                    ITmfStateValue state = value.getStateValue();
+//                    retMap.put(Messages.ControlFlowView_attributeSyscallName, state.toString());
+//                }
+//
+//            } catch (AttributeNotFoundException e) {
+//            	CtracePlugin.getDefault().logError("Error in ControlFlowPresentationProvider", e); //$NON-NLS-1$
+//            } catch (TimeRangeException e) {
+//            	CtracePlugin.getDefault().logError("Error in ControlFlowPresentationProvider", e); //$NON-NLS-1$
+//            } catch (StateSystemDisposedException e) {
+//                /* Ignored */
+//            }
+//        }
+
+        return retMap;
+    }
+
+    @Override
+    public void postDrawEvent(ITimeEvent event, Rectangle bounds, GC gc) {
+        if (bounds.width <= gc.getFontMetrics().getAverageCharWidth()) {
+            return;
+        }
+        if (!(event instanceof TimeEvent)) {
+            return;
+        }
+        ControlFlowEntry entry = (ControlFlowEntry) event.getEntry();
+        ITmfStateSystem ss = TmfStateSystemAnalysisModule.getStateSystem(entry.getTrace(), CtraceAnalysisModule.ID);
+        if (ss == null) {
+            return;
+        }
+        int status = ((TimeEvent) event).getValue();
+
+        /* Fix me, RH */
+//        if (status != StateValues.PROCESS_STATUS_RUN_SYSCALL) {
+//            return;
+//        }
+//        try {
+//            int syscallQuark = ss.getQuarkRelative(entry.getThreadQuark(), Attributes.SYSTEM_CALL);
+//            ITmfStateInterval value = ss.querySingleState(event.getTime(), syscallQuark);
+//            if (!value.getStateValue().isNull()) {
+//                ITmfStateValue state = value.getStateValue();
+//                gc.setForeground(gc.getDevice().getSystemColor(SWT.COLOR_WHITE));
+//                Utils.drawText(gc, state.toString().substring(4), bounds.x, bounds.y - 2, bounds.width, true, true);
+//            }
+//        } catch (AttributeNotFoundException e) {
+//        	CtracePlugin.getDefault().logError("Error in ControlFlowPresentationProvider", e); //$NON-NLS-1$
+//        } catch (TimeRangeException e) {
+//        	CtracePlugin.getDefault().logError("Error in ControlFlowPresentationProvider", e); //$NON-NLS-1$
+//        } catch (StateSystemDisposedException e) {
+//            /* Ignored */
+//        }
+    }
+}
---
+++ ctrace/net.sf.etrakr.tmf.ctrace.test/src/net/sf/etrakr/tmf/ctrace/test/DummyTest.java
@@ -0,0 +1,38 @@
+package net.sf.etrakr.tmf.ctrace.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DummyTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+		String str = "";
+		Assert.assertNotNull(str);
+		
+	}
+
+}
