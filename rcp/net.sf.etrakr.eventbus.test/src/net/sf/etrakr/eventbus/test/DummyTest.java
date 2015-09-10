0f88f777b78567e8539c24909d201fd2ef48642e
ranhow.chen <ranhow.chen@gmail.com>
2015-09-11 01:00:13 +0800

    Add eventbus test plugin
    	modified:   pom.xml
    	modified:   rcp/net.sf.etrakr.eventbus.test/META-INF/MANIFEST.MF
    	modified:   rcp/net.sf.etrakr.eventbus.test/pom.xml
    	modified:   rcp/net.sf.etrakr.eventbus.test/src/net/sf/etrakr/eventbus/test/DummyTest.java
    	modified:   rcp/net.sf.etrakr.eventbus/build.properties
    	modified:   rcp/net.sf.etrakr.eventbus/src/net/sf/etrakr/eventbus/EventBus.java

--- rcp/net.sf.etrakr.eventbus.test/src/net/sf/etrakr/eventbus/test/DummyTest.java
+++ rcp/net.sf.etrakr.eventbus.test/src/net/sf/etrakr/eventbus/test/DummyTest.java
@@ -5,12 +5,20 @@ package net.sf.etrakr.eventbus.test;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.e4.core.services.events.IEventBroker;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventHandler;
+
+import net.sf.etrakr.eventbus.EventBus;
 
 /**
  * @author admin
@@ -51,6 +59,26 @@ public class DummyTest {
 		
 		String str = "";
 		Assert.assertNotNull(str);
+	}
+	
+	@Test
+	public void test1() {
+		
+		EventBus.registerEvent(new EventHandler(){
+
+			@Override
+			public void handleEvent(Event event) {
+				
+				System.out.println("DummyTest.EventHandler : "+ event);
+			}
+			
+		}, "org/eclipse/equinox/events/systrace/*");
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put(IEventBroker.DATA, 2); 		
+		
+		Event event = new Event("org/eclipse/equinox/events/systrace/CRITICAL", map);
+		EventBus.getEventBus().postEvent(event);
 	}
 
 }
--- rcp/net.sf.etrakr.eventbus/src/net/sf/etrakr/eventbus/EventBus.java
+++ rcp/net.sf.etrakr.eventbus/src/net/sf/etrakr/eventbus/EventBus.java
@@ -1,6 +1,12 @@
 package net.sf.etrakr.eventbus;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
 import org.osgi.service.event.EventAdmin;
+import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
 
 public class EventBus {
 
@@ -16,6 +22,17 @@ public class EventBus {
 		return eventBus;
 	}
 
+	public static void registerEvent(EventHandler handler, String topic) {
+
+		String[] topics = new String[] { topic };
+		
+		Hashtable<String, String[]> ht = new Hashtable<String, String[]>();
+		
+		ht.put(EventConstants.EVENT_TOPIC, topics);
+		
+		EventbusActivator.getContext().registerService(EventHandler.class.getName(), handler, ht);
+
+	}
 	/**
 	 * Method will be used by DS to set the
 	 * <code>org.osgi.service.event.EventAdmin</code> service.
